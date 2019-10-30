19111993f94df8b0e2c5c82305992f57c1c0ffab
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-10-30 03:07:27 -0400

    Cassandra

--- Hetionet/Cassandra.py
+++ Hetionet/Cassandra.py
@@ -1,73 +1,40 @@
-import logging
-
-log = logging.getLogger()
-log.setLevel('DEBUG')
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
-log.addHandler(handler)
-
-from cassandra import ConsistencyLevel
+import json
 from cassandra.cluster import Cluster
-from cassandra.query import SimpleStatement
+from cassandra.auth import PlainTextAuthProvider
 
-KEYSPACE = "testkeyspace"
 
-def main():
-    cluster = Cluster(['127.0.0.1'])
+"""
+Get credentials from Kubernetes. The credentials were set up as a dictionary. For example:
+{
+    "username": "USERNAME",
+    "password": "PASSWORD"
+}
+"""
+credentials = None
+with open('/var/run/secrets/user_credentials/cassandra_credentials') as f:
+    credentials = json.load(f)
+# Verify the credentials were pulled correctly
+if credentials:
+    # Setup authentication mechanism
+    auth_provider = PlainTextAuthProvider(
+        username=credentials.get('username'),
+        password=credentials.get('password')
+    )
+
+    # Pass parameters to the cluster
+    cluster = Cluster(
+        auth_provider=auth_provider,
+        contact_points=['support-cassandra.dev.anaconda.com']
+    )
+
+    # Connect to cluster and set the keyspace
     session = cluster.connect()
+    session.set_keyspace('quote')
 
-    rows = session.execute("SELECT keyspace_name FROM system.schema_keyspaces")
-    if KEYSPACE in [row[0] for row in rows]:
-        log.info("dropping existing keyspace...")
-        session.execute("DROP KEYSPACE " + KEYSPACE)
-
-    log.info("creating keyspace...")
-    session.execute("""
-        CREATE KEYSPACE %s
-        WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
-        """ % KEYSPACE)
-
-    log.info("setting keyspace...")
-    session.set_keyspace(KEYSPACE)
-
-    log.info("creating table...")
-    session.execute("""
-        CREATE TABLE mytable (
-            thekey text,
-            col1 text,
-            col2 text,
-            PRIMARY KEY (thekey, col1)
-        )
-        """)
-
-    query = SimpleStatement("""
-        INSERT INTO mytable (thekey, col1, col2)
-        VALUES (%(key)s, %(a)s, %(b)s)
-        """, consistency_level=ConsistencyLevel.ONE)
-
-    prepared = session.prepare("""
-        INSERT INTO mytable (thekey, col1, col2)
-        VALUES (?, ?, ?)
-        """)
-
-    for i in range(10):
-        log.info("inserting row %d" % i)
-        session.execute(query, dict(key="key%d" % i, a='a', b='b'))
-        session.execute(prepared.bind(("key%d" % i, 'b', 'b')))
-
-    future = session.execute_async("SELECT * FROM mytable")
-    log.info("key\tcol1\tcol2")
-    log.info("---\t----\t----")
-
-    try:
-        rows = future.result()
-    except Exception:
-        log.exeception()
-
+    # Run query in keyspace and print out the results
+    rows = session.execute('SELECT * FROM historical_prices')
     for row in rows:
-        log.info('\t'.join(row))
+        print(row)
 
-    session.execute("DROP KEYSPACE " + KEYSPACE)
-
-if __name__ == "__main__":
-    main()
+    # Disconnect from the cluster
+    cluster.shutdown()
