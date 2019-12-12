e1a8b8a330ea312f8668469e581b6996f3d4fb4a
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-12-12 13:36:17 -0500

    implement tf

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,5 +1,9 @@
 
 def make_matrix(project2_test):
+   """
+   :param project2_test:
+   :return: matrix
+   """
    with open(project2_test, 'r') as file:
        columns = []
        for document in file.readlines():
@@ -8,23 +12,60 @@ def make_matrix(project2_test):
 
 
 def filter_document(document):
+   """
+   :param document:
+   :return: filtered_document
+   """
    row = []
    for term in document.split():
-      if document.split()[0] not in row:
-         row.append(document.split()[0])
+      # if document.split()[0] not in row:
+      #    row.append(document.split()[0])
       if term.startswith('gene') and term.endswith('gene') or term.startswith('dis') and term.endswith('dis'):
          row.append(term)
    return row
 
 
 def print_matrix(matrix):
+   """
+   :param matrix:
+   :return: None
+   """
    for row in matrix:
-      print(row, "\n")
+      # print(row, "\n")
+      print(len(row), row, "\n")
+
+
+def tf(document):
+   """
+   :param document:
+   :return: tf
+   """
+   for word in document:
+      if 'gene' in word:
+         print(word)
+      if 'dis' in word:
+         print(word)
+   pass
+
+
+def tf_idf():
+   """
+   :return: TF-IDF
+   """
+   pass
+
+
+def semantic_similarity():
+   """
+   :return:
+   """
+   pass
 
 
 if __name__ == "__main__":
    matrix = make_matrix("project2_test.txt")
-   print_matrix(matrix)
+   # print_matrix(matrix)
+   tf(matrix[0])
 
 
 
