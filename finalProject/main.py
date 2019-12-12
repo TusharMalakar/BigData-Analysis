bb3dbec25b16353072ea0220a7e755e2973bff84
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-12-12 02:36:12 -0500

    perser added

--- finalProject/main.py
+++ finalProject/main.py
@@ -8,3 +8,19 @@ with open("project2_test.txt", 'r') as file:
             if word.startswith('gene') and word.endswith('gene') or word.startswith('dis') and word.endswith('dis'):
                 docList.append(word)
 
+# print(docList)
+numDoc = []
+for ascii in docList:
+   if 57 >= ord(ascii[0]) >= 48:
+      numDoc.append(ord(ascii[0]))
+
+matrix = []
+for i in range(len(numDoc)):
+   row = []
+   for j, tr in zip(range(20), docList):
+      if ord(tr[0]) <=47 or ord(tr[0]) >= 58:
+         row.append(tr)
+   matrix.append(row)
+print(matrix)
+
+
