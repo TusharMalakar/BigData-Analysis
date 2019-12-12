5203cf12247706ad786974f782dfbdfda9ff7186
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-12-12 01:05:51 -0500

    list created

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,8 +1,10 @@
 
-
-
+docList = []
 with open("project2_test.txt", 'r') as file:
     for document in file.readlines():
         for word in document.split():
+            if document.split()[0] not in docList:
+                docList.append(document.split()[0])
             if word.startswith('gene') and word.endswith('gene') or word.startswith('dis') and word.endswith('dis'):
-                print(document.split()[0], word)
+                docList.append(word)
+
