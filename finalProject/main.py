b80084c543961779736490553cd86212aa79cb6b
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-12-11 23:32:18 -0500

    perser added

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,79 +1,8 @@
-from pyspark import SparkContext
-from operator import add
 
 
-sc5 = SparkContext("local", "Collect app")
-words = sc5.parallelize(
-   ["scala",
-   "java",
-   "hadoop",
-   "spark",
-   "akka",
-   "spark vs hadoop",
-   "pyspark",
-   "pyspark and spark"]
-)
-coll = words.collect()
-print("Elements in RDD -> %s" % (coll))
 
-
-# sc4 = SparkContext("local", "Filter app")
-# words = sc4.parallelize (
-#    ["scala",
-#    "java",
-#    "hadoop",
-#    "spark",
-#    "akka",
-#    "spark vs hadoop",
-#    "pyspark",
-#    "pyspark and spark"]
-# )
-# words_filter = words.filter(lambda x: 'spark' in x)
-# filtered = words_filter.collect()
-# print("Fitered RDD -> %s" % (filtered))
-#
-#
-# sc3 = SparkContext("local", "Map app")
-# words = sc4.parallelize (
-#    ["scala",
-#    "java",
-#    "hadoop",
-#    "spark",
-#    "akka",
-#    "spark vs hadoop",
-#    "pyspark",
-#    "pyspark and spark"]
-# )
-# words_map = words.map(lambda x: (x, 1))
-# mapping = words_map.collect()
-# print ("Key value pair -> %s" % (mapping))
-#
-#
-# sc2 = SparkContext("local", "Reduce app")
-# nums = sc2.parallelize([1, 2, 3, 4, 5])
-# adding = nums.reduce(add)
-# print("Adding all the elements -> %i" % (adding))
-#
-#
-# sc0 = SparkContext("local", "Join app")
-# x = sc0.parallelize([("spark", 1), ("hadoop", 4)])
-# y = sc0.parallelize([("spark", 2), ("hadoop", 5)])
-# joined = x.join(y)
-# final = joined.collect()
-# print("Join RDD -> %s" % (final))
-#
-#
-# sc1 = SparkContext("local", "Cache app")
-# words = sc1.parallelize (
-#    ["scala",
-#    "java",
-#    "hadoop",
-#    "spark",
-#    "akka",
-#    "spark vs hadoop",
-#    "pyspark",
-#    "pyspark and spark"]
-# )
-# words.cache()
-# caching = words.persist().is_cached
-# print("Words got chached > %s" % (caching))
+with open("project2_test.txt", 'r') as file:
+    for document in file.readlines():
+        for word in document.split():
+            if word.startswith('gene') and word.endswith('gene') or word.startswith('dis') and word.endswith('dis'):
+                print(document.split()[0], word)
--- finalProject/sparkStreaming.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-
-
-# Create a local StreamingContext with two working thread and batch interval of 1 second
-sc = SparkContext("local[2]", "NetworkWordCount")
-ssc = StreamingContext(sc, 1)
